a407fa08392b15f3c5ffb27a582e7de9a87dcec7
Valentiu77 <44373573+Valentiu77@users.noreply.github.com>
2018-11-05 20:14:12 +0200

    preper for function checkExtractPermission() {

--- js/learn.js
+++ js/learn.js
@@ -26,15 +26,65 @@ sayWelcome();
 
 //schimb valutar
 
-var cursEUR = 4.7;
+var cursEUR = 4.5;
 var myEUR = 20;
 var myRON = 0;
-var friendEUR = 20;
+var friendEUR = 100;
 var friendRON = 0;
 //operatori sunt [* / + - ]
 
-myRON = myRON * 4.7;
+myRON = myEUR * cursEUR;
 console.info("eu am " + myRON + " RON ");
 
-friendRON = friendEUR * 4.7;
+friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
 console.info("tu ai " + friendRON + " RON ");
+
+var ATMFunds = 50000;
+var funds = 5000;
+
+function getExtractFee(amount) {
+  var comision = amount * 0.01;
+  if (comision < 2.5) {
+    console.warn("comision minim de aplica");
+    comision = 2.5;
+  }
+  return comision;
+}
+//TODO implement
+function checkExtractPermission() {
+  return true;
+}
+
+function extractFromATM(amount) {
+  console.info(`= = = =  = = = = =  = = = = =  = = = = `);
+  console.info(`suma extrasa este:` + amount);
+  var comision = getExtractFee(amount);
+
+  var totalExtract = amount + comision;
+
+  if (totalExtract > funds) {
+    console.error("insufficeint funds");
+    return;
+  }
+  if (ATMFunds < totalExtract) {
+    console.error("insufficent funds");
+    return;
+  }
+
+  funds = funds - totalExtract;
+  // ATMFunds = ATMFunds - ammount;
+  ATMFunds -= amount;
+  console.info(`comision aplicat:` + comision);
+  console.info("sold curent: " + funds);
+  console.info(`= = = =  = = = = =  = = = = =  = = = = `);
+}
+
+extractFromATM(1005); // nu e multiplu de zece
+extractFromATM(100000); //solduri insuficiente
+extractFromATM(3000);
+extractFromATM(3000);
+extractFromATM(100);
+extractFromATM(0); //
+
+// // Sa facem alt extras, cati bani am,
+// daca sold insuficeint, cat mai multe if-uri, sa faca calul pas cu pas
