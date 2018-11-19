c70275c87bfea5482474349c88f4cab818ea32d8
Your Name <valentiu.king@gmail.com>
2018-11-19 19:58:11 +0200

    add education

--- src/index.js
+++ src/index.js
@@ -11,20 +11,30 @@ function show (id) {
 function showHomePage () {
     hide('skills-page');
     hide('languages-page');
+    hide('education-page');
     show('home-page');
 }
 function showSkillsPage () {
     hide('home-page');
     hide('languages-page');
+    hide('education-page');
     show('skills-page');
+}
+function showEducationpage () {
+    hide('home-page');
+    hide('skills-page');
+    hide('languages-page');
+    show('education-page');
 }
 function showLanguagesPage () {
     hide('home-page');
     hide('skills-page');
+    hide('education-page');
     show('languages-page');
 }
 
 $("home-menu").onclick = showHomePage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationpage;
 $("languages-menu").onclick = showLanguagesPage;
 
